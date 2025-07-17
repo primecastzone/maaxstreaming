23599a71b91364fdd8db75a2232f4ad96cbc916f
primecastzone <vinitasukheja1@gmail.com>
2025-07-17 16:38:49 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'max providers'
 
 
 # The full version, including alpha/beta/rc tags
